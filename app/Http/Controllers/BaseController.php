452e6a0bfe8033753f2e913637b36dc341afae01
Burashev Shamil <shburashev@ya.ru>
2022-04-23 16:52:28 +0300

    add: BaseController for send responses

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse($message, $result, $code = 200): \Illuminate\Http\JsonResponse
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, $code);
+    }
+
+
+    /**
+     * error response method.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendError($error, $errorMessages = [], $code = 404): \Illuminate\Http\JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
