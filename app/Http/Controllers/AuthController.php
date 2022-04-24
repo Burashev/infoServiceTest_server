8b4d915bed2d31cfbb75487c28037ef3e1a5fb50
Burashev Shamil <shburashev@ya.ru>
2022-04-24 12:31:44 +0300

    add: user registration and authorization

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class AuthController extends BaseController
 {
@@ -18,11 +23,37 @@ class AuthController extends BaseController
             return $this->sendError('Validation error', $validator->errors(), 400);
         }
 
-        return $this->sendResponse('', null, 201);
+        $user = User::query()->create([
+           'email' => $request->input('email'),
+           'password' => Hash::make($request->input('password')),
+           'token' => Str::random(32)
+        ]);
+
+        event(new Registered($user));
+
+        return $this->sendResponse('', $user, 201);
     }
 
     public function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|exists:users',
+            'password' => 'required'
+        ]);
 
+        if ($validator->fails()) {
+            return $this->sendError('Validation error', $validator->errors(), 400);
+        }
+
+        $attempt = Auth::attempt($request->only(['email', 'password']));
+
+        if (!$attempt) {
+            return $this->sendError('Email or password is incorrect', null, 401);
+        }
+
+        $user = Auth::user();
+        $user->update(['token' => Str::random(32)]);
+
+        return $this->sendResponse('Successful login', $user, 200);
     }
 
     public function verificationNotice() {
@@ -30,13 +61,13 @@ class AuthController extends BaseController
     }
 
     public function verificationVerify(EmailVerificationRequest $request) {
-        $request->fulfill();
-
         if ($request->user()->hasVerifiedEmail()) {
             return $this->sendError('User was already verified', null, 400);
         }
 
-        return $this->sendResponse('Successful verification!', null, 201);
+        $request->fulfill();
+
+        return $this->sendResponse('Successful verification!', null, 200);
     }
 
     public function verificationSend(Request $request) {
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'token',
         'email',
         'password',
     ];
