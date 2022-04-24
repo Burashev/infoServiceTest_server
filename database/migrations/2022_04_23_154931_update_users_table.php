9b456c8e52f96d520db61bb45e33e3986aa681a5
Burashev Shamil <shburashev@ya.ru>
2022-04-24 11:26:48 +0300

    add: update users table migration

---
+++ database/migrations/2022_04_23_154931_update_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('token')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumns('users', ['email_verified_at', 'token']);
+    }
+};
