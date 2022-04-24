a6711d7498004e0a6756658764c682c9f07a506c
Burashev Shamil <shburashev@ya.ru>
2022-04-24 13:47:13 +0300

    add: application controller

---
+++ app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class ApplicationController extends BaseController
+{
+    public function createApplication(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'number' => 'required',
+            'company' => 'required',
+            'application_name' => 'required',
+            'message' => 'required',
+            'file' => 'required|file',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation error', $validator->errors(), 400);
+        }
+
+        $file_name = Str::random() . '.' . $request->file('file')->clientExtension();
+        $request->file('file')->move( public_path() . '/upload_files',  $file_name);
+
+        $application = Application::query()->make($validator->validated());
+        $application->file ='/upload_files/' . $file_name;
+
+        Auth::user()->applications()->save($application);
+
+        return $this->sendResponse('Application successful created', $application, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ApplicationController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth.api')->group(function () {
-    Route::get('/email/verify', [AuthController::class, 'verificationNotice'])
-        ->name('verification.notice');
+    Route::prefix('/email')->group(function() {
+        Route::get('/verify', [AuthController::class, 'verificationNotice'])
+            ->name('verification.notice');
 
-    Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verificationVerify'])
-        ->middleware('signed')->name('verification.verify');
+        Route::get('/verify/{id}/{hash}', [AuthController::class, 'verificationVerify'])
+            ->middleware('signed')->name('verification.verify');
 
-    Route::post('/email/verification-notification', [AuthController::class, 'verificationSend'])
-        ->middleware('throttle:6,1')->name('verification.send');
+        Route::post('/verification-notification', [AuthController::class, 'verificationSend'])
+            ->middleware('throttle:6,1')->name('verification.send');
+    });
 
+    Route::prefix('/application')->group(function() {
+        Route::post('/create', [ApplicationController::class, 'createApplication']);
+
+    });
 });
