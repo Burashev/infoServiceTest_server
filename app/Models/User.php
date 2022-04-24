52af715879f4d75de6c3181b39958c5625e53ae4
Burashev Shamil <shburashev@ya.ru>
2022-04-24 13:49:29 +0300

    add: applications one to many relationship in user

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function applications() {
+        return $this->hasMany(Application::class);
+    }
 }
