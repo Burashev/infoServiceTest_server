818dd51b1a8b4c3e0912e6c83e896fa82c9c59ac
Burashev Shamil <shburashev@ya.ru>
2022-04-24 13:02:30 +0300

    add: create_applications_table migration

---
+++ database/migrations/2022_04_24_095807_create_applications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('number');
+            $table->string('company');
+            $table->string('application_name');
+            $table->string('message');
+            $table->string('file');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+};
